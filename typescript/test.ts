095d096978a1da11146f2b52fb678b2811b31b91
yourcarrot0214 <stylenbs@gmail.com>
2021-09-10 23:25:40 +0900

    함수 타입

--- typescript/test.ts
+++ typescript/test.ts
@@ -1,16 +1,33 @@
-enum Color {
-  Red = "red",
-  Green = "green",
-  Blue = "blue",
+interface SquareConfig {
+  color: string;
+  width?: number;
 }
 
-let c: Color = Color.Blue;
-console.log(c);
-
-function error(message: string): never {
-  throw new Error(message);
+interface ReturnSquare {
+  color: string;
+  area: number;
 }
 
-let someValue1: any = "this is a string";
+function createSquare(config: SquareConfig): ReturnSquare {
+  let newSquare = { color: "orange", area: 1000 };
+  if (config.color) {
+    newSquare.color = config.color;
+  }
+  if (config.width) {
+    newSquare.area = config.width * config.width;
+  }
 
-let strLength1: number = (<string>someValue).length;
+  return newSquare;
+}
+
+let mySquare: ReturnSquare = createSquare({ color: "black", width: 100 });
+let mySquare2 = createSquare({ color: "orange", opacity: 100 } as SquareConfig);
+
+interface SearchFunc {
+  (source: string, subString: string): boolean;
+}
+
+let mySearch: SearchFunc = function (source, subString): boolean {
+  let result = source.search(subString);
+  return result > -1;
+};
