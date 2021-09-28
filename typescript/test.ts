199c2fdead2f3538ee51c475e2c306a6977a9dfd
yourcarrot0214 <stylenbs@gmail.com>
2021-09-28 19:38:23 +0900

    TS : Generic

--- typescript/test.ts
+++ typescript/test.ts
@@ -1,33 +1,39 @@
-interface SquareConfig {
-  color: string;
-  width?: number;
-}
-
-interface ReturnSquare {
-  color: string;
-  area: number;
-}
-
-function createSquare(config: SquareConfig): ReturnSquare {
-  let newSquare = { color: "orange", area: 1000 };
-  if (config.color) {
-    newSquare.color = config.color;
+class Orange {
+  private name = "Orange";
+  constructor(private brix: number = 0) {}
+  getName(): string {
+    return this.name;
   }
-  if (config.width) {
-    newSquare.area = config.width * config.width;
+  getBrix(): number {
+    return this.brix;
   }
-
-  return newSquare;
 }
 
-let mySquare: ReturnSquare = createSquare({ color: "black", width: 100 });
-let mySquare2 = createSquare({ color: "orange", opacity: 100 } as SquareConfig);
-
-interface SearchFunc {
-  (source: string, subString: string): boolean;
+class Apple {
+  private name = "Apple";
+  constructor(private brix: number = 0) {}
+  getName(): string {
+    return this.name;
+  }
+  getBrix(): number {
+    return this.brix;
+  }
 }
 
-let mySearch: SearchFunc = function (source, subString): boolean {
-  let result = source.search(subString);
-  return result > -1;
-};
+class Box<T> {
+  constructor(private fruit: T) {}
+  getFruit(): T {
+    return this.fruit;
+  }
+}
+
+const box: Box<Orange> = new Box(new Orange(5));
+console.log(box.getFruit().getName());
+console.log(box);
+
+// Generic을 지정하지 않은 상태로 string 값을 넘겨주면 자동으로 <String> 추론 된다.
+// 이에 stringBox는 compile을 pass하게 된다.
+const stringBox = new Box("string");
+// string이 가지고 있지 않은 getName() 메서드 호출로 compile error가 발생한다.
+// console.log(stringBox.getFruit().getName());
+console.log(stringBox);
