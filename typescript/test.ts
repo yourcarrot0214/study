f4106017dcbbb19789c4b6a8d9a9e189d430c036
yourcarrot0214 <stylenbs@gmail.com>
2021-09-30 20:50:49 +0900

    Generic 예제 정의

--- typescript/test.ts
+++ typescript/test.ts
@@ -34,3 +34,81 @@ let cardPicker = deck.createCardPicker();
 let pickedCard = cardPicker();
 
 console.log("card: " + pickedCard.card + " of " + pickedCard.suit);
+
+type Job = "developer" | "marketer" | "degisner";
+type User = {
+  name: string;
+  job: Job;
+};
+
+const user1: User = {
+  name: "Carrot",
+  job: "developer",
+};
+
+const user2: User = {
+  name: "Wilted",
+  job: "marketer",
+};
+
+function getSize<T>(arr: T[]) {
+  return arr.length;
+}
+
+let stringArray: string[] = ["a", "b", "c"];
+let objectArray: object[] = [{}, {}, {}];
+
+getSize<object>(objectArray);
+getSize<string>(stringArray);
+
+interface Mobile<T> {
+  name: string;
+  price: number;
+  option: T;
+}
+
+interface Option {
+  color: string;
+  version: number;
+}
+
+const apple: Mobile<Option> = {
+  name: "iPhone",
+  price: 990,
+  option: {
+    color: "red",
+    version: 13,
+  },
+};
+
+const samsung: Mobile<string> = {
+  name: "galaxy",
+  price: 990,
+  option: "getting ready",
+};
+
+interface User {
+  name: string;
+  age: number;
+}
+
+interface Car {
+  name: string;
+  color: string;
+}
+
+interface Book {
+  price: number;
+}
+
+const user: User = { name: "Carrot", age: 35 };
+const car: Car = { name: "Tesla", color: "white" };
+const book: Book = { price: 39000 };
+
+function printName<T extends { name: string }>(data: T): string {
+  return data.name;
+}
+
+printName(user);
+printName(car);
+printName(book);
